92774056563ea4ceac19a5225447530674093ee8
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-05-02 13:34:02 +0000

    Fix minor error

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,7 @@ function runGame() {
     // Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+}
 
 function checkAnswer() {
 
